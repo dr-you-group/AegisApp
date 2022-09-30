63f4d962e711ac0be6d1a29890778d17347edf4b
Sungjae Jung <sungjae.2425@gmail.com>
2022-09-30 17:20:49 +0900

    update ui components

--- R/database.R
+++ R/database.R
@@ -1,6 +1,5 @@
 database_ui <- fluidPage(
-  textInput("dbms", "Database dialect", "sql server"),
-  textOutput("dbms"),
+  selectInput("dbms", "Database dialect", c("sql server")),
   textInput("path_to_driver", "JDBC path", getwd()),
   textInput("connection_string", "Database connection string", "jdbc:sqlserver://IP:PORT;user=ID;password=PW"),
   textInput("cdm_database_schema", "CDM Database schema", "DB.SCHEMA"),
@@ -8,11 +7,30 @@ database_ui <- fluidPage(
 
   actionButton("save_db_info", "Save!"),
   actionButton("get_cohort_list", "Get cohort list"),
-  actionButton("init_db_info", "Initialize")
+  actionButton("init_db_info", "Initialize"),
+
+  textOutput("conn_info")
 )
 
 database_server <- function(input, output, session) {
-  output$dbms <- renderText({
-    input$dbms
+  output$conn_info <- renderText({
+    param <- list()
+    param$conn$dbms <- input$dbms
+    param$conn$path_to_driver <- input$path_to_driver
+    param$conn$connection_string <- input$connection_string
+
+    message(
+      "message param: ",
+      toString(param)
+    )
+
+    conn_info <- get_connection_details(param)
+
+    message(
+      "message conn_info: ",
+      toString(conn_info)
+    )
+
+    toString(conn_info)
   })
 }
