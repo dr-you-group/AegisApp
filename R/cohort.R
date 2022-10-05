a7775555a57ab9d5a652ee3d12ef7ddff09185f0
Sungjae Jung <sungjae.2425@gmail.com>
2022-10-05 15:41:53 +0900

    update functions

--- R/cohort.R
+++ R/cohort.R
@@ -3,7 +3,7 @@ cohort_tab <- tabsetPanel(
   type = "tabs",
 
   tabPanel(
-    title = "Define cohort",
+    title = "Get cohort table",
 
     selectInput("target_cohort_definition_id", "Target cohort definition id", c(1:10)),
     selectInput("outcome_cohort_definition_id", "Outcome cohort definition id", c(1:10)),
@@ -14,33 +14,42 @@ cohort_tab <- tabsetPanel(
     radioButtons("time_at_risk_end_date_panel", "Time at risk end date panel", c("cohort_start_date", "cohort_end_date")),
 
     actionButton("print_cohort", "Print"),
+    actionButton("get_cohort_table", "Get cohort table"),
+
+    dataTableOutput("cohort_table")
   ),
   tabPanel(
-    title = "Select geo data",
+    title = "Get geo data",
 
     selectInput("name", "Source of the geo data", c("GADM", "KOR")),
     selectInput("country", "Country", c("KOR")),
     radioButtons("level", "Level of the administrative data", c(2:3)),
 
     actionButton("print_geo", "Print"),
+    actionButton("get_geo", "Get geo data"),
+
+    dataTableOutput("geo")
   ),
   tabPanel(
-    title = "Set adjustment",
+    title = "Get adjustmented table",
 
     selectInput("mode", "Adjustment mode", c("Std", "Crd")),
     selectInput("fraction", "Fraction", c("100000")),
     selectInput("conf_level", "Confidence level", c("0.95")),
 
     actionButton("print_adj", "Print"),
+    actionButton("get_table_adj", "Get adjustmented table"),
+
+    dataTableOutput("table_adj")
   ),
   tabPanel(
-    title = "Get cohort table",
+    title = "Plot disease map/cluster",
 
-    actionButton("get_cohort", "Get cohort table"),
+    # actionButton("get_cohort", "Get cohort table"),
     actionButton("plot_disease_map", "Plot disease map"),
     actionButton("plot_disease_cluster", "Plot disease cluster"),
 
-    dataTableOutput("cohort")
+    # dataTableOutput("cohort")
   )
 )
 
@@ -83,7 +92,7 @@ cohort_server <- function(input, output, session) {
 
   })
 
-  cohort <- eventReactive(input$get_cohort, {
+  cohort_table <- eventReactive(input$get_cohort_table, {
     # Get connection details
     param <- base::list()
     param$conn$dbms <- input$dbms
@@ -108,7 +117,15 @@ cohort_server <- function(input, output, session) {
 
     cohort_table <- get_cohort_analysis_table(param)
 
+    cohort_table
+  })
 
+  output$cohort_table <- renderDataTable(
+    cohort_table(),
+    options = list(pageLength = 5)
+  )
+
+  geo <- eventReactive(input$get_geo, {
     # Get geo data
     param <- base::list()
     param$geo$name <- input$name
@@ -117,11 +134,19 @@ cohort_server <- function(input, output, session) {
 
     geo <- get_geo_data(param)
 
+    geo
+  })
 
+  output$geo <- renderDataTable(
+    geo(),
+    options = list(pageLength = 5)
+  )
+
+  table_adj <- eventReactive(input$get_table_adj, {
     # Map cohort table with geo data
     param <- base::list()
-    param$latlong <- cohort_table
-    param$geo <- geo
+    param$latlong <- cohort_table()
+    param$geo <- geo()
 
     geo_map <- map_latlong_geo(param)
 
@@ -145,31 +170,89 @@ cohort_server <- function(input, output, session) {
     table_adj
   })
 
-  output$cohort <- renderDataTable(
-    cohort(),
+  output$table_adj <- renderDataTable(
+    table_adj(),
     options = list(pageLength = 5)
   )
 
+  # cohort <- eventReactive(input$get_cohort, {
+  #   # Get connection details
+  #   param <- base::list()
+  #   param$conn$dbms <- input$dbms
+  #   param$conn$path_to_driver <- input$path_to_driver
+  #   param$conn$connection_string <- input$connection_string
+  #
+  #   conn_info <- get_connection_details(param)
+  #
+  #
+  #   # Get cohort table
+  #   param <- base::list()
+  #   param$conn_info <- conn_info
+  #   param$query$cdm_database_schema <- input$cdm_database_schema
+  #   param$query$result_database_schema <- input$result_database_schema
+  #   param$query$target_cohort_definition_id <- input$target_cohort_definition_id
+  #   param$query$outcome_cohort_definition_id <- input$outcome_cohort_definition_id
+  #   param$query$cohort_start_date <- input$cohort_start_date
+  #   param$query$cohort_end_date <- input$cohort_end_date
+  #   param$query$time_at_risk_start_date <- input$time_at_risk_start_date
+  #   param$query$time_at_risk_end_date <- input$time_at_risk_end_date
+  #   param$query$time_at_risk_end_date_panel <- input$time_at_risk_end_date_panel
+  #
+  #   cohort_table <- get_cohort_analysis_table(param)
+  #
+  #
+  #   # Get geo data
+  #   param <- base::list()
+  #   param$geo$name <- input$name
+  #   param$geo$country <- input$country
+  #   param$geo$level <- input$level
+  #
+  #   geo <- get_geo_data(param)
+  #
+  #
+  #   # Map cohort table with geo data
+  #   param <- base::list()
+  #   param$latlong <- cohort_table
+  #   param$geo <- geo
+  #
+  #   geo_map <- map_latlong_geo(param)
+  #
+  #
+  #   # Arrange table
+  #   param <- base::list()
+  #   param$table <- geo_map
+  #
+  #   table_arr <- calculate_count_with_geo_oid(param)
+  #
+  #
+  #   # Adjustment for age and sex
+  #   param <- base::list()
+  #   param$table <- table_arr
+  #   param$adj$mode <- input$mode
+  #   param$adj$fraction <- input$fraction
+  #   param$adj$conf_level <- input$conf_level
+  #
+  #   table_adj <- calculate_adjust_age_sex_indirectly(param)
+  #
+  #   table_adj
+  # })
+  #
+  # output$cohort <- renderDataTable(
+  #   cohort(),
+  #   options = list(pageLength = 5)
+  # )
+
   disease_map <- eventReactive(input$plot_disease_map, {
-    # Get geo data
-    param <- base::list()
-    param$geo$name <- input$name
-    param$geo$country <- input$country
-    param$geo$level <- input$level
-
-    geo <- get_geo_data(param)
-
-
     # Generate graph file
     param <- base::list()
-    param$geo <- geo
+    param$geo <- geo()
 
     graph_file_path <- trans_geo_to_graph(param)
 
 
     # Calculate disease map
     param <- base::list()
-    param$table <- cohort()
+    param$table <- table_adj()
     param$graph_file_path <- graph_file_path
 
     deriv <- calculate_disease_map(param)
@@ -177,7 +260,7 @@ cohort_server <- function(input, output, session) {
 
     # Merge geo data with derivatives
     param <- base::list()
-    param$geo <- geo
+    param$geo <- geo()
     param$deriv <- deriv$arranged_table
 
     data <- merge_geo_with_deriv(param)
@@ -199,25 +282,16 @@ cohort_server <- function(input, output, session) {
   )
 
   disease_cluster <- eventReactive(input$plot_disease_cluster, {
-    # Get geo data
-    param <- base::list()
-    param$geo$name <- input$name
-    param$geo$country <- input$country
-    param$geo$level <- input$level
-
-    geo <- get_geo_data(param)
-
-
     # Calculate disease cluster
     param <- base::list()
-    param$table <- cohort()
+    param$table <- table_adj()
 
     deriv <- calculate_disease_cluster(param)
 
 
     # Merge geo data with derivatives
     param <- base::list()
-    param$geo <- geo
+    param$geo <- geo()
     param$deriv <- deriv$arranged_table
 
     data <- merge_geo_with_deriv(param)
