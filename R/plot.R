2ccbb7cb39c6d9b6b41ad42cb61c78472f56d696
Sungjae Jung <sungjae.2425@gmail.com>
2022-10-07 01:20:05 +0900

    remove not used files

--- R/plot.R
+++
@@ -1,9 +0,0 @@
-plot_ui <- function(id) {
-  plotOutput(NS(id, "plot"), width = "400px")
-}
-
-plot_server <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    output$plot <- renderPlot(plot(1:5), res = 96)
-  })
-}
